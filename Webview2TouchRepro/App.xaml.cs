d0b09c1a7820e227340e6ec101b55a492b0f1b6e
dkorzeniewski <dkorzeniewski@empire-today.com>
2025-11-12 13:43:45 -0600

    Add project files.

---
+++ Webview2TouchRepro/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Webview2TouchRepro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Webview2TouchRepro" };
+        }
+    }
+}
---
+++ Webview2TouchRepro/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Webview2TouchRepro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
